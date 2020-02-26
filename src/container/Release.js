385288ac6e85d20d10058207ad14919eb1a4c8dd
welliton <josealves_w@yahoo.com.br>
2020-02-26 01:14:52 -0300

    criando lista de release

--- src/container/Release.js
+++ src/container/Release.js
@@ -2,10 +2,59 @@ import React, { Component } from 'react';
 
 export default class Release extends Component {
 
-    render(){
-        return(
-            <h1>Release</h1>
-        );
+    constructor(){
+        super();
+        this.state = {
+            releases:[
+                {
+                    id: 1,
+                    releaseName: 'Release 01',
+                    releaseDate: '01/01/2017'
+                },
+                {
+                    id: 2,
+                    releaseName: 'Release 02',
+                    releaseDate: '01/01/2018'
+                },
+                {
+                    id: 3,
+                    releaseName: 'Release 03',
+                    releaseDate: '01/01/2019'
+                },
+                {
+                    id: 4,
+                    releaseName: 'Release 04',
+                    releaseDate: '01/01/2020'
+                }
+            ]
+        }
     }
+
+    render(){
+         return(
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.releases.map((release, index) =>{
+                    return(
+                    <tr key={release.id}>
+                    <th scope="row">{release.id}</th>
+                    <td>{release.releaseName}</td>
+                    <td>{release.releaseDate}</td>
+                    <td><button type="button" className="btn btn-danger btn-sm">Remover</button></td>
+                    </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+                    );
+                }
 
 }
